0948986111262760384eed4c108e92c79935c450
SHRISHAILOFFICIAL <shrishailkone@outlook.com>
2025-11-18 18:41:47 +0530

    Fix CI/CD coverage: use pytest and exclude test files from coverage calculation

--- conftest.py
+++ conftest.py
@@ -1,5 +1,14 @@
+import os
 import django
 from django.conf import settings
 
-def pytest_configure():
+# Configure Django settings before tests run
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helping_hand_core.settings')
+
+def pytest_configure(config):
+    """Configure pytest for Django."""
+    if not settings.configured:
+        django.setup()
     settings.DEBUG = False
+
+
